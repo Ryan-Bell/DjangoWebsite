75fdb220cc547e7daf7d64dd5a4b1964e163de7f
Ryan Bell <42rbell@gmail.com>
2015-10-17 16:30:59 -0400

    Added comments and cleaned urls.py

--- HealthNet/HealthNet/urls.py
+++ HealthNet/HealthNet/urls.py
@@ -19,7 +19,23 @@ from HealthApp import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+"""
+The urlpatterns is how we map the site urls to specific views in the views.py. The first part is
+a regular expression to describe the url pattern, followed by the view that should be called.
+Lastly, a name is given to each pattern so that they can be referenced from elsewhere in the code.
+For example, when an HTTPResponseRedirect(reverse('login')) is returned in one of the views, it
+is doing a reverse lookup of the url pattern named 'login' and returning the view (and subsequently
+the html page) associated with the view.
 
+There are a couple patterns that are a bit unique. The first is the url for the admin page which
+links to the built in url network already created by django. The other unique urls are the ones
+that deal with patient information since the urls are specific to the patient, and the username in
+the url needs to be passed into the view as a parameter. The format of (?P<username>\w+) is used
+to first identify that information is being captured, and to identify what parameter it is being passed
+in as (in this case, the username parameter).
+
+Note: the first url is used to redirect users to the login page when at the 'root' url of the site.
+"""
 urlpatterns = [
     url(r'^$', views.userLogin, name='login'),
     url(r'^login/$', views.userLogin, name='login'),
@@ -30,16 +46,3 @@ urlpatterns = [
     url(r'^(?P<username>\w+)/profile/$', views.profile, name='profile'),
     url(r'^(?P<username>\w+)/profile/Appointments$', views.profileAppointments, name='calendar')
 ]
-
-
-
-
-'''
-urlpatterns = [
-    url(r'^$', loginPage),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', loginPage),
-    url(r'^patientRegistration/$', patientRegister),
-    url(r'^patientProfile/(?P<username>\w{0,50})/$', patientProfile),
-]
-'''
