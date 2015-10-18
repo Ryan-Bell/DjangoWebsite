b152cc93d925c571167bfbd2f1fa1386afb71ed9
Ryan Bell <42rbell@gmail.com>
2015-10-18 00:51:00 -0400

    Commented Models

--- HealthNet/HealthApp/models.py
+++ HealthNet/HealthApp/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser
 from abc import ABCMeta, abstractclassmethod
+"""
+The models.py is essentially where objects are declared. Currently, It holds the
+PatientProfile object and the logItem object. These objects are most always referenced
+in the views. In each class, fields are declared by giving the name followed by the type
+of input field. Many of the models have a OneToOne link to another model. ManyToOne,
+OneToMany, and ManyToMany are also other types of multiplicities allowed. These link the models.
+"""
 
+#I don't exactly remember what this is for or why  past-Ryan created it and never
+#used it. Nonetheless, I remember thinking it was a good idea at the time so I
+#decided not to delete it for now.
 TYPES = (
     ('Patient', 'Patient'),
     ('Doctor', 'Nurse'),
@@ -9,6 +19,7 @@ TYPES = (
     ('Admin', 'Admin')
 )
 
+#This is used to limit the entry boxes to 50 characters
 MAX_LENGTH = 50
 
 class Profile(models.Model):
@@ -16,6 +27,7 @@ class Profile(models.Model):
     #type = models.CharField(max_length=10, types=TYPES)
     type = models.CharField(max_length=10)
     firstName = models.CharField(max_length=MAX_LENGTH)
+    #setting blank to true means this field will not be required
     middleName = models.CharField(max_length=MAX_LENGTH, blank=True)
     lastName = models.CharField(max_length=MAX_LENGTH)
     email = models.EmailField(blank=False)
@@ -25,7 +37,7 @@ class Profile(models.Model):
 	
     def __str__(self):
         return self.user.username
-
+    #custom methods can be defined
     def getName(self):
         return self.firstName + " " + self.lastName
 
