157153c8f730ff20c5e915fea4ec58091973a43b
Ryan Bell <42rbell@gmail.com>
2015-10-24 16:57:23 -0400

    Added Profile, Medical Info, and Insurance Info to models
    Front end is still currently broken as backend is changing

--- HealthNet/HealthApp/models.py
+++ HealthNet/HealthApp/models.py
@@ -12,16 +12,82 @@ OneToMany, and ManyToMany are also other types of multiplicities allowed. These 
 #I don't exactly remember what this is for or why  past-Ryan created it and never
 #used it. Nonetheless, I remember thinking it was a good idea at the time so I
 #decided not to delete it for now.
-TYPES = (
-    ('Patient', 'Patient'),
-    ('Doctor', 'Doctor'),
-    ('Nurse', 'Nurse'),
-    ('Admin', 'Admin')
+#TYPES = (
+#    ('Patient', 'Patient'),
+#    ('Doctor', 'Doctor'),
+#   ('Nurse', 'Nurse'),
+#   ('Admin', 'Admin')
+#)
+
+STATE_CHOICES = (
+    ('AL', 'Alabama'),
+    ('AK', 'Alaska'),
+    ('AZ', 'Arizona'),
+    ('AR', 'Arkansas'),
+    ('CA', 'California'),
+    ('CO', 'Colorado'),
+    ('CT', 'Connecticut'),
+    ('DE', 'Delaware'),
+    ('DC', 'District of Columbia'),
+    ('FL', 'Florida'),
+    ('GA', 'Georgia'),
+    ('HI', 'Hawaii'),
+    ('ID', 'Idaho'),
+    ('IL', 'Illinois'),
+    ('IN', 'Indiana'),
+    ('IA', 'Iowa'),
+    ('KS', 'Kansas'),
+    ('KY', 'Kentucky'),
+    ('LA', 'Louisiana'),
+    ('ME', 'Maine'),
+    ('MD', 'Maryland'),
+    ('MA', 'Massachusetts'),
+    ('MI', 'Michigan'),
+    ('MN', 'Minnesota'),
+    ('MS', 'Mississippi'),
+    ('MO', 'Missouri'),
+    ('MT', 'Montana'),
+    ('NE', 'Nebraska'),
+    ('NV', 'Nevada'),
+    ('NH', 'New Hampshire'),
+    ('NJ', 'New Jersey'),
+    ('NM', 'New Mexico'),
+    ('NY', 'New York'),
+    ('NC', 'North Carolina'),
+    ('ND', 'North Dakota'),
+    ('OH', 'Ohio'),
+    ('OK', 'Oklahoma'),
+    ('OR', 'Oregon'),
+    ('PA', 'Pennsylvania'),
+    ('RI', 'Rhode Island'),
+    ('SC', 'South Carolina'),
+    ('SD', 'South Dakota'),
+    ('TN', 'Tennessee'),
+    ('TX', 'Texas'),
+    ('UT', 'Utah'),
+    ('VT', 'Vermont'),
+    ('VA', 'Virginia'),
+    ('WA', 'Washington'),
+    ('WV', 'West Virginia'),
+    ('WI', 'Wisconsin'),
+    ('WY', 'Wyoming'),
+)
+
+SUPPORTED_INSURANCE = (
+    ('ExampleKey', 'ExampleValue')
 )
 
 #This is used to limit the entry boxes to 50 characters
 MAX_LENGTH = 50
 
+class InsuranceInfo(models.Model):
+    provider = models.CharField(choices=SUPPORTED_INSURANCE)
+    policyNumber = models.CharField(max_length=MAX_LENGTH)
+    groupNumber = models.CharField(max_length=MAX_LENGTH)
+    policyExpirationDate = models.DateField()
+    premiumAmount = models.CharField(max_length=MAX_LENGTH)
+    POLICY_TYPE = (('Primary', 'Primary'),('Secondary','Secondary'))
+    policyType = models.CharField(choices=POLICY_TYPE)
 
 class MedicalInfo(models.Model):
     tuberculosis = models.BooleanField(default=False)
@@ -51,51 +117,85 @@ class MedicalInfo(models.Model):
     anemia = models.BooleanField(default=False)
     obesity = models.BooleanField(default=False)
     epilepsy = models.BooleanField(default=False)
+    #add cancer with multiple selector
+    #add diabetes with selector
+    #possible fields such as last checkup etc.
 
 class Profile(models.Model):
+    firstName = models.CharField()
+    middleName = models.CharField(blank=True)
+    middleInitial = models.CharField(max_length=1)
+    lastName = models.CharField()
+    socialSecurity = models.CharField(max_length=9)
+    citizen = models.BooleanField(default=True)
+    dateOfBirth = models.DateField(blank=True)
+    MALE = 'M'
+    FEMALE = 'F'
+    SEX_CHOICES = ((MALE,'Male'),(FEMALE,'Female'),)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES,default=MALE)
+    address = models.CharField(max_length=MAX_LENGTH)
+    city = models.CharField(max_length=MAX_LENGTH)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES)
+    zipcode = models.CharField(max_length=5)
+    phoneNumber = models.CharField(max_length=14)
+
+
+    email = models.EmailField(blank=False)
+    #switch to email based login
+    #choice of preferred hospital
+    #choice of emergency contact info (linked to other patient if already in system)
+
 
 class Patient(models.Model):
     user = models.OneToOneField(User)
+    profileInfo = models.OneToOneField(Profile, null=True)
+    insuranceInfo = models.OneToOneField(InsuranceInfo, null=True)
     medicalInfo = models.OneToOneField(MedicalInfo, null=True)
     doctor = models.ForeignKey(Doctor)
     #type = models.CharField(max_length=10, types=TYPES)
-    type = models.CharField(max_length=10)
-    firstName = models.CharField(max_length=MAX_LENGTH)
+    #type = models.CharField(max_length=10)
     #setting blank to true means this field will not be required
-    middleName = models.CharField(max_length=MAX_LENGTH, blank=True)
-    lastName = models.CharField(max_length=MAX_LENGTH)
-    email = models.EmailField(blank=False)
-    phoneNumber = models.CharField(max_length=14)
-    dateOfBirth = models.DateField(blank=True)
-    address = models.CharField(max_length=MAX_LENGTH, blank=True)
+
 
     def __str__(self):
         return self.user.username
     #custom methods can be defined
     def getName(self):
-        return self.firstName + " " + self.lastName
+        return self.profileInfo.firstName + " " + self.profileInfo.lastName
 
-    def getType(self):
-        return self.type
+    #def getType(self):
+    #    return self.type
 
 
 class Doctor(models.Model):
     user = models.OneToOneField(User)
+    profileInfo = models.OneToOneField(Profile, null=True)
 
 class Nurse(models.Model):
     user = models.OneToOneField(User)
+    profileInfo = models.OneToOneField(Profile, null=True)
 
 class Admin(models.Model):
     user = models.OneToOneField(User)
+    profileInfo = models.OneToOneField(Profile, null=True)
 
 class Hospital(models.Model):
     #rooms list - possibly a room model with basic information
+    #method for adding room
+    #method for removing room
+
 
 #class Perscription??
 
 #class Logmanager? with many methods?
-#the logmanager would also keep track of the statistics
-#and viewing/sorting as well as making pretty graphs
+    #the logmanager would also keep track of the statistics
+    #and viewing/sorting as well as making pretty graphs
+    #methods for adding/retrieving logitems and sorting
+
+#classes for the various tests that can be preformed with appropriate fields
+#in addition, an accompaning form will need to be created
+#all tests classes can inherit from a base test class that the patient will hold
+#a list of
 
 class LogItem(models.Model):
 	user = models.OneToOneField(User)
